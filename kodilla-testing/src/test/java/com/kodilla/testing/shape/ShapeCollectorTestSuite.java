9d320ba19051f38614cc53ac9ed9600f7a8d3dc9
unknown <Fordon89@o2.pl>
2020-02-23 21:45:45 +0100

    6.4

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -12,11 +12,12 @@ public class ShapeCollectorTestSuite {
         shapeCollector.addFigure(new Circle());
         shapeCollector.addFigure(new Triangle());
         //Then
-        Assert.assertTrue("Circle".equals(shapeCollector.getFigure(0).getShapeName()));
+        Assert.assertTrue(shapeCollector.getFigure(0) instanceof Shape);
+        //Assert.assertTrue("Circle".equals(shapeCollector.getFigure(0).getShapeName()));
         //When
         shapeCollector.addFigure(new Circle());
         //Then
-        Assert.assertTrue("Triangle".equals(shapeCollector.getFigure(1).getShapeName()));
+        Assert.assertTrue(shapeCollector.getFigure(1) instanceof Shape);
     }
 
     @Test
@@ -32,7 +33,7 @@ public class ShapeCollectorTestSuite {
         //When
         shapeCollector.removeFigure(circle);
         //Then
-        Assert.assertTrue("Triangle".equals(shapeCollector.getFigure(1).getShapeName()));
+        Assert.assertTrue(shapeCollector.getFigure(1) instanceof Shape);
     }
 
     @Test
@@ -45,11 +46,11 @@ public class ShapeCollectorTestSuite {
         //When
         Shape shape = shapeCollector.getFigure(1);
         //Then
-        Assert.assertTrue("Square".equals(shape.getShapeName()));
+        Assert.assertTrue(shape instanceof Shape);
         //When
         shape = shapeCollector.getFigure(2);
         //Then
-        Assert.assertTrue("Triangle".equals(shape.getShapeName()));
+        Assert.assertTrue(shape instanceof Shape);
     }
 
 }
