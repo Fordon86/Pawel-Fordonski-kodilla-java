70b09eb40a01d49b71f562b3a424152aceb9c1d2
Fordon <fordon-86@o2.pl>
2020-04-16 20:16:36 +0200

    Task 14.4

---
+++ kodillla-spring-web/src/main/java/com/kodillla/spring/web/KodilllaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodillla.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilllaSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodilllaSpringWebApplication.class, args);
+    }
+
+}
---
+++ kodillla-spring-web/src/test/java/com/kodillla/spring/web/KodilllaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillla.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodilllaSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
