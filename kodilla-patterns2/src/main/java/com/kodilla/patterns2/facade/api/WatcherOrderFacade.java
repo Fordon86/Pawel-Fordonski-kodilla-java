ca5e22bc2469288543394f198c5c28156fa43a01
Fordon <fordon-86@o2.pl>
2020-07-17 20:26:43 +0200

    Task 24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/WatcherOrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/WatcherOrderFacade.java
@@ -3,7 +3,6 @@ package com.kodilla.patterns2.facade.api;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,22 +12,14 @@ import org.springframework.stereotype.Component;
 public class WatcherOrderFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(WatcherOrderFacade.class);
 
-/*    @Before("execution( * com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
-    public void logEvent() {
-        LOGGER.info("Order saved");
-    }*/
+    @Around("execution( * com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..) )" +
+            "&& args (orderDto, userId) ")
+    public Object logEvent (final ProceedingJoinPoint proceedingJoinPoint, OrderDto orderDto, Long userId) throws Throwable {
 
-    @Around("execution( * com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
-//            && args(savedLog)")
-    public Object logEvent (final ProceedingJoinPoint joinPoint) throws Throwable {
         try {
-            Object result = joinPoint.proceed();
-            System.out.println("Log: " + (joinPoint.getTarget()));
-            System.out.println("Log: " + joinPoint.getArgs().toString());
-            System.out.println("Log: " + joinPoint.getClass().getName());
-            ///System.out.println("dsdas" + joinPoint);
+            Object result = proceedingJoinPoint.proceed();;
+            LOGGER.info("User ID is: " + userId);
             return result;
-
         } catch (Throwable throwable) {
             LOGGER.error(throwable.getMessage());
             throw throwable;
